695390d60a7fa80b789c07d74c7ab3d4456f3792
Ole Jørgen Bakken <olejorgenbakken@gmail.com>
2020-02-05 17:08:01 +0100

    fixed issues

--- src/assets/patients.js
+++ src/assets/patients.js
@@ -1,6 +1,7 @@
 const patients = [
 	{
-		name: 'Ole Jørgen Bakken',
+		firstName: 'Emil',
+		lastName: 'I Lønneberget',
 		disease: 'Influenza',
 		img:
 			'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-YJtU4lyRMoQ%2FUzHk4IOM1tI%2FAAAAAAAALRQ%2FRs0ldgEOUSc%2Fs1600%2FEemeli02.jpg&f=1&nofb=1'
