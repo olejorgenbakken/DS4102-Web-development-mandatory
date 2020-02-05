4d05a3b29e1086166edf12db699d5aa29c86fd97
Haakon Hess <haakon.hess@gmail.com>
2020-02-05 17:14:35 +0100

    Added objects

--- src/assets/patients.js
+++ src/assets/patients.js
@@ -1,10 +1,61 @@
 const patients = [
 	{
-		name: 'Ole Jørgen Bakken',
+		firstName: 'Ole Jørgen',
+		lastName: 'Bakken',
+		age: '22',
 		disease: 'Influenza',
 		img:
 			'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-YJtU4lyRMoQ%2FUzHk4IOM1tI%2FAAAAAAAALRQ%2FRs0ldgEOUSc%2Fs1600%2FEemeli02.jpg&f=1&nofb=1'
-	}
+	},
+	{
+		firstName: 'Reodor',
+		lastName: 'Felgen',
+		age: '22',
+		disease: 'Broken Toe',
+		img: 'https://dbstatic.no/62230562.jpg?imageId=62230562&panow=100.00&panoh=100.00&panox=0&panoy=0&heightw=100.00&heighth=100.00&heightx=0&heighty=0&width=1200&height=630'
+	},
+	{
+		firstName: 'Petter',
+		lastName: 'Oes',
+		age: '76',
+		disease: 'Colls',
+		img: ''
+	},
+	{
+		firstName: 'Bendik',
+		lastName: 'Te',
+		age: '34',
+		disease: 'Stomach ache',
+		img: ''
+	},
+	{
+		firstName: 'Ben Redik',
+		lastName: 'Fy Fazan',
+		age: '56',
+		disease: 'Apelicious Infectus',
+		img: ''
+	},
+	{
+		firstName: 'Knut',
+		lastName: 'Havervik',
+		age: '54',
+		disease: 'Bruton Agammaglobulinemia',
+		img: ''
+	},
+	{
+		firstName: 'Jarle',
+		lastName: 'Oesterstad',
+		age: '30',
+		disease: 'Schnitzler Syndrome',
+		img: ''
+	},
+	{
+		firstName: 'Birgitt',
+		lastName: 'Veikernes',
+		age: '72',
+		disease: 'Pediatric Severe Combined Immunodeficiency',
+		img: ''
+	},
 ];
 
 export default {
